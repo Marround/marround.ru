4edaf82dfb56973158f62001aa90b9d2617b267e
Marround <marround@yandex.ru>
2018-01-25 18:36:07 +0300

    update price component

--- src/app/sections/services/staticpages/price/price.component.ts
+++ src/app/sections/services/staticpages/price/price.component.ts
@@ -3,7 +3,6 @@ import {IPricelist} from '../../../../srvice/pricelist';
 import {LoadJsonService} from '../../../../srvice/loadjson.service';
 import {makeStateKey, Meta, Title, TransferState} from '@angular/platform-browser';
 import {HttpClient} from '@angular/common/http';
-import {Response} from '@angular/http';
 
 const PRICE_KEY = makeStateKey('price');
 
@@ -15,11 +14,16 @@ const PRICE_KEY = makeStateKey('price');
 })
 export class PriceComponent implements OnInit {
   pricelist: IPricelist;
-  // priceUrl: string;
-  //
-  // errorMessage: string;
+  priceUrl: string;
+  errorMessage: string;
 
-  constructor(private title: Title, private meta: Meta, private http: HttpClient, private state: TransferState) {
+  constructor(
+    private jsonService: LoadJsonService,
+    private title: Title,
+    private meta: Meta,
+    private http: HttpClient,
+    private state: TransferState
+  ) {
     this.title.setTitle('Прайслист - Услуги - Marround - частный вэб мастер - Белгород');
     this.meta.updateTag({name: 'keywords', content: 'Прайслист, цены, тарифы, разработка сайтов, создание сайта, сопровождение сайта, публикация сайта, Marround, частный вэб мастер, Белгород'});
     this.meta.updateTag({name: 'description', content: 'Прайслист, полный перечень оказываемых услуг'});
@@ -28,20 +32,23 @@ export class PriceComponent implements OnInit {
   ngOnInit() {
     this.pricelist = this.state.get(PRICE_KEY, null as any);
 
-    if (!this.pricelist) {
-      this.http
-        .get('http://marround.ru/assets/json/price.json')
-        .map((response: Response) => response.json())
-        .subscribe(data => {
-          data.then(value => {
-            this.pricelist = value.pricelist;
-            this.state.set(PRICE_KEY, value.pricelist as any);
-          });
-        });
-    }
+    this.priceUrl = 'http://' + location.hostname + '/assets/json/price.json';
+    this.jsonService.getJSON(this.priceUrl).subscribe(data => {
+      this.pricelist = data.pricelist;
+      this.state.set(PRICE_KEY, data.pricelist as any);
+    }, error => this.errorMessage = < any > error);
 
-    // this.priceUrl = 'http://' + location.hostname + '/assets/json/price.json';
-    // this.jsonService.getJSON(this.priceUrl).subscribe(data => this.pricelist = data.pricelist, error => this.errorMessage = < any > error);
+  // if (!this.pricelist) {
+  //   this.http
+  //     .get('http://marround.ru/assets/json/price.json')
+  //     .map((response: Response) => response.json())
+  //     .subscribe(data => {
+  //       data.then(value => {
+  //         this.pricelist = value.pricelist;
+  //         this.state.set(PRICE_KEY, value.pricelist as any);
+  //       });
+  //     });
+  // }
   }
 
 }
