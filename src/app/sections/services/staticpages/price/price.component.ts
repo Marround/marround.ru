699250244938a4b2f7139de06705ee7c67d653a8
Marround <marround@yandex.ru>
2018-01-25 17:45:49 +0300

    update price component

--- src/app/sections/services/staticpages/price/price.component.ts
+++ src/app/sections/services/staticpages/price/price.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {IPricelist} from '../../../../srvice/pricelist';
 import {LoadJsonService} from '../../../../srvice/loadjson.service';
-import {Meta, Title} from '@angular/platform-browser';
+import {makeStateKey, Meta, Title, TransferState} from '@angular/platform-browser';
+import {HttpClient} from '@angular/common/http';
 
+const PRICE_KEY = makeStateKey('price');
 
 @Component({
   moduleId: module.id,
@@ -11,22 +13,34 @@ import {Meta, Title} from '@angular/platform-browser';
   styleUrls: ['./price.component.css']
 })
 export class PriceComponent implements OnInit {
-
   pricelist: IPricelist;
+  // priceUrl: string;
+  //
+  // errorMessage: string;
 
-  priceUrl: string;
-
-  errorMessage: string;
-
-  constructor(private jsonService: LoadJsonService, private title: Title, private meta: Meta) {
+  constructor(private title: Title, private meta: Meta, private http: HttpClient, private state: TransferState) {
     this.title.setTitle('Прайслист - Услуги - Marround - частный вэб мастер - Белгород');
     this.meta.updateTag({name: 'keywords', content: 'Прайслист, цены, тарифы, разработка сайтов, создание сайта, сопровождение сайта, публикация сайта, Marround, частный вэб мастер, Белгород'});
     this.meta.updateTag({name: 'description', content: 'Прайслист, полный перечень оказываемых услуг'});
   }
 
   ngOnInit() {
-    this.priceUrl = 'http://marround.ru/assets/json/price.json';
-    this.jsonService.getJSON(this.priceUrl).subscribe(data => this.pricelist = data.pricelist, error => this.errorMessage = < any > error);
+    this.pricelist = this.state.get(PRICE_KEY, null as any);
+
+    if (!this.pricelist) {
+      this.http
+        .get('http://marround.ru/assets/json/price.json')
+        .map((response: Response) => response.json())
+        .subscribe(data => {
+          data.then(value => {
+            this.pricelist = value.pricelist;
+            this.state.set(PRICE_KEY, value.pricelist as any);
+          });
+        });
+    }
+
+    // this.priceUrl = 'http://' + location.hostname + '/assets/json/price.json';
+    // this.jsonService.getJSON(this.priceUrl).subscribe(data => this.pricelist = data.pricelist, error => this.errorMessage = < any > error);
   }
 
 }
