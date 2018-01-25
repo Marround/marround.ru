0f6df9542305dce6d5ac17e169134c28b2f8cb7a
Marround <marround@yandex.ru>
2018-01-25 17:09:55 +0300

    update pricelink IV

--- src/app/sections/services/staticpages/price/price.component.ts
+++ src/app/sections/services/staticpages/price/price.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {IPricelist} from '../../../../srvice/pricelist';
 import {LoadJsonService} from '../../../../srvice/loadjson.service';
 import {Meta, Title} from '@angular/platform-browser';
-import {Router} from '@angular/router';
+
 
 @Component({
   moduleId: module.id,
@@ -14,20 +14,19 @@ export class PriceComponent implements OnInit {
 
   pricelist: IPricelist;
 
-  priceUrl: 'http://marround.ru/assets/json/price.json';
+  priceUrl: string;
 
   errorMessage: string;
 
-  constructor(private jsonService: LoadJsonService, private title: Title, private meta: Meta, private router: Router) {
+  constructor(private jsonService: LoadJsonService, private title: Title, private meta: Meta) {
     this.title.setTitle('Прайслист - Услуги - Marround - частный вэб мастер - Белгород');
     this.meta.updateTag({name: 'keywords', content: 'Прайслист, цены, тарифы, разработка сайтов, создание сайта, сопровождение сайта, публикация сайта, Marround, частный вэб мастер, Белгород'});
     this.meta.updateTag({name: 'description', content: 'Прайслист, полный перечень оказываемых услуг'});
   }
 
   ngOnInit() {
-
+    this.priceUrl = 'http://marround.ru/assets/json/price.json';
     this.jsonService.getJSON(this.priceUrl).subscribe(data => this.pricelist = data.pricelist, error => this.errorMessage = < any > error);
-
   }
 
 }
