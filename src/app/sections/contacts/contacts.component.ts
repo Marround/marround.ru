51ac5828c4c431762828abb38b3555b39225d5d0
Marround <marround@yandex.ru>
2018-02-18 17:23:38 +0300

    add micro-markup

--- src/app/sections/contacts/contacts.component.ts
+++ src/app/sections/contacts/contacts.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Meta, Title} from '@angular/platform-browser';
 
 declare let $: any;
@@ -14,7 +14,7 @@ class Message {
   templateUrl: './contacts.component.html',
   styleUrls: ['./contacts.component.css']
 })
-export class ContactsComponent implements OnInit {
+export class ContactsComponent implements OnInit, OnDestroy {
   message: Message;
   uName: string;
   email: string;
@@ -23,14 +23,49 @@ export class ContactsComponent implements OnInit {
 
   constructor(private title: Title, private meta: Meta) {
     this.title.setTitle('Контакты - Marround');
-    this.meta.updateTag({name: 'keywords', content: 'Контакты, телефон, номер, skype, telegram, Marround'});
-    this.meta.updateTag({name: 'description', content: 'Контактные данные Marround. 8(920) 205-51-25'});
+    this.meta.updateTag({name: 'description', content: '☑ Контактные данные Marround. Тел: 8(920) 205-51-25, Telegram: @marround, Skype: marround, VK: vk.com/marround.'});
+
+    this.meta.updateTag({property: 'og:locale', content: 'ru_RU'});
+    this.meta.updateTag({property: 'og:title', content: 'Контакты - Marround'});
+    this.meta.updateTag({
+      property: 'og:description',
+      content: '☑ Контактные данные Marround. Тел: 8(920) 205-51-25, Telegram: @marround, Skype: marround, VK: vk.com/marround.'
+    });
+    this.meta.updateTag({property: 'og:type', content: 'website'});
+    this.meta.updateTag({property: 'og:url', content: 'http://marround.ru/contacts'});
+    this.meta.updateTag({property: 'og:site_name', content: 'Marround'});
+
+    this.meta.updateTag({name: 'mobile-web-app-capable', content: 'yes'});
+    this.meta.updateTag({name: 'apple-mobile-web-app-capable', content: 'yes'});
+    this.meta.updateTag({name: 'application-name', content: 'Marround'});
+    this.meta.updateTag({name: 'apple-mobile-web-app-title', content: 'Marround'});
+    this.meta.updateTag({name: 'theme-color', content: '#A4C04E'});
+    this.meta.updateTag({name: 'msapplication-navbutton-colo', content: '#A4C04E'});
+    this.meta.updateTag({name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent'});
+    this.meta.updateTag({name: 'msapplication-starturl', content: 'http://marround.ru/'});
   }
 
   ngOnInit() {
     this.toTop();
   }
+  ngOnDestroy() {
+    this.meta.removeTag('property="og:locale"');
+    this.meta.removeTag('property="og:title"');
+    this.meta.removeTag('property="og:description"');
+    this.meta.removeTag('property="og:type"');
+    this.meta.removeTag('property="og:url"');
+    this.meta.removeTag('property="og:image"');
+    this.meta.removeTag('property="og:site_name"');
 
+    this.meta.removeTag('name="mobile-web-app-capable"');
+    this.meta.removeTag('name="apple-mobile-web-app-capable"');
+    this.meta.removeTag('name="application-name"');
+    this.meta.removeTag('name="apple-mobile-web-app-title"');
+    this.meta.removeTag('name="theme-color"');
+    this.meta.removeTag('name="msapplication-navbutton-colo"');
+    this.meta.removeTag('name="apple-mobile-web-app-status-bar-style"');
+    this.meta.removeTag('name="msapplication-starturl"');
+  }
   toTop() {
     $('body,html').animate({
       scrollTop: 0
--- src/app/sections/services/main/main.component.ts
+++ src/app/sections/services/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Meta, Title} from '@angular/platform-browser';
 
 declare let $: any;
@@ -8,24 +8,59 @@ declare let $: any;
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, OnDestroy {
 
   constructor(private title: Title, private meta: Meta) {
     this.title.setTitle('Услуги - Marround');
     this.meta.updateTag({
-      name: 'keywords',
-      content: 'создать сайт, разработка сайтов, разработка мобильныйх сайтов, разработка сайтов под ключ, ' +
-      'разработка официалоьношго сайта, СЕО оптимизация, продвижение сайтов, поддержка сайтов, низкая цена'
-    });
-    this.meta.updateTag({
       name: 'description',
-      content: 'Услуги на разработку, сопровождение и СЕО продвижение сайтов. ' +
+      content: '☑ Услуги на разработку, сопровождение и СЕО продвижение сайтов. ' +
       'Разработка создание сайтов, лэндинг, одностраничкик, сайт визитка, корпоративный сайт, сайт каталог. ' +
       'Низкая цена: от 3000 р. Индивидуальный подход к каждому клиету. Сопровождение сайта.'});
+
+    this.meta.updateTag({property: 'og:locale', content: 'ru_RU'});
+    this.meta.updateTag({property: 'og:title', content: 'Услуги - Marround'});
+    this.meta.updateTag({
+      property: 'og:description',
+      content: '☑ Услуги на разработку, сопровождение и СЕО продвижение сайтов. ' +
+      'Разработка создание сайтов, лэндинг, одностраничкик, сайт визитка, корпоративный сайт, сайт каталог. ' +
+      'Низкая цена: от 3000 р. Индивидуальный подход к каждому клиету. Сопровождение сайта.'
+    });
+    this.meta.updateTag({property: 'og:type', content: 'website'});
+    this.meta.updateTag({property: 'og:url', content: 'http://marround.ru/services'});
+    this.meta.updateTag({property: 'og:image', content: 'http://marround.ru/assets/img/texture/texture_3.png'});
+    this.meta.updateTag({property: 'og:site_name', content: 'Marround'});
+
+    this.meta.updateTag({name: 'mobile-web-app-capable', content: 'yes'});
+    this.meta.updateTag({name: 'apple-mobile-web-app-capable', content: 'yes'});
+    this.meta.updateTag({name: 'application-name', content: 'Marround'});
+    this.meta.updateTag({name: 'apple-mobile-web-app-title', content: 'Marround'});
+    this.meta.updateTag({name: 'theme-color', content: '#A4C04E'});
+    this.meta.updateTag({name: 'msapplication-navbutton-colo', content: '#A4C04E'});
+    this.meta.updateTag({name: 'apple-mobile-web-app-status-bar-style', content: 'black-translucent'});
+    this.meta.updateTag({name: 'msapplication-starturl', content: 'http://marround.ru/'});
   }
 
   ngOnInit() {
     this.toTop();
+  }
+  ngOnDestroy() {
+    this.meta.removeTag('property="og:locale"');
+    this.meta.removeTag('property="og:title"');
+    this.meta.removeTag('property="og:description"');
+    this.meta.removeTag('property="og:type"');
+    this.meta.removeTag('property="og:url"');
+    this.meta.removeTag('property="og:image"');
+    this.meta.removeTag('property="og:site_name"');
+
+    this.meta.removeTag('name="mobile-web-app-capable"');
+    this.meta.removeTag('name="apple-mobile-web-app-capable"');
+    this.meta.removeTag('name="application-name"');
+    this.meta.removeTag('name="apple-mobile-web-app-title"');
+    this.meta.removeTag('name="theme-color"');
+    this.meta.removeTag('name="msapplication-navbutton-colo"');
+    this.meta.removeTag('name="apple-mobile-web-app-status-bar-style"');
+    this.meta.removeTag('name="msapplication-starturl"');
   }
   toTop() {
     $('body,html').animate({
